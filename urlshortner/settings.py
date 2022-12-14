409acb5fa4942e37cee439978566a4967100b840
momemtwithace <augustineezekiel763@gmail.com>
2022-12-15 00:13:29 +0100

    Added heroku deployment dependency

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nbe@u))d@kle&bi1tmt6)-6k8omsjh)$3*-38cahoq$=gn=dto'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
